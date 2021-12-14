08f2cb53d3589b3f5525558fcc9869253e8612ee
Ceejay Pimentel <ceejaypimentel@hotmail.ca>
2021-12-13 21:55:33 -0600

    skinet: Section 2

---
+++ core/Entities/Product.cs
@@ -0,0 +1,8 @@
+namespace core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
